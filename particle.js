5754b937cad522cc14e98f5c3d5358abbbbfcb41
Joshua Mitchell <joshuamit2003@gmail.com>
2024-05-15 14:03:13 -0400

    Fixing application text boxes

--- particle.js
+++ particle.js
@@ -1,5 +1,28 @@
-function testFunc() {
-const ctx = myCanvas.getContext("2d");
-ctx.fillRect(20,20,150,100);
-alert("heyyy");
+function startApp() {
+    var c = document.getElementById("myCanvas");
+    const ctx = c.getContext("2d");
+    
+    var x = document.createElement("INPUT");
+    x.type = "text";
+    var y = document.createElement("INPUT");
+    y.type = "text";
+    x.value = "x-coordinate";
+    y.value = "y-coordinate";
+    document.body.appendChild(x);
+    document.body.appendChild(y);
+    //document.body.appendChild(createParticleButton]);
+
+/*
+    const element = document.getElementById("div1");
+    element.appendChild(x);
+    element.appendChild(y);
+    //element.appendChild(createParticleButton);
+    
+    const para = document.createElement("p");
+    const node = document.createTextNode("This is new.");
+    para.appendChild(node);
+
+    const element = document.getElementById("div1");
+    element.appendChild(para);
+*/
 }
